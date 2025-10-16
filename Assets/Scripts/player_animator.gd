1fff9c8c2103f6ed6ffeac3c8af0705f0e0c831b
Rey Oronos, Jr. <36144771+SirReyyy@users.noreply.github.com>
2025-10-16 16:24:00 +0800

    Player Animations
    - added new animations to player: idle, walk, run, jump and fall

---
+++ Assets/Scripts/player_animator.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+# export variables
+@export var player_controller : PlayerController
+@export var animation_player : AnimationPlayer
+@export var sprite : Sprite2D
+
+# process function
+func _process(delta):
+	# player sprite flip
+	if player_controller.direction == 1:
+		sprite.flip_h = false
+	elif player_controller.direction == -1:
+		sprite.flip_h = true
+		
+	# player walk animation
+	if abs(player_controller.velocity.x) > 0.0:
+		animation_player.play("walk_animation")
+	else:
+		animation_player.play("idle_animation")
+		
+	# player jump animation
+	if player_controller.velocity.y < 0.0:
+		animation_player.play("jump_animation")
+	elif player_controller.velocity.y > 0.0:
+		animation_player.play("fall_animation")
--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name PlayerController
 
 @export var speed = 20.0
 @export var jump_power = 35.0
